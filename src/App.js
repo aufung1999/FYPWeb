1a1b5d83ab8260d1685e0a8592d095e233bf995b
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-03-14 14:00:12 -0400

    1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const handleSubmit = async (e) => {
+      const body = new FormData();
+      body.append("file", e.target.file);
+      const res = await fetch('http://127.0.0.1:8000/predict', { method: "POST", body });
+      return res.ok ? res : console.log('failed to upload: ');
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        {/* {data?.map((each_data, index) => (
+          <div key={index}>{each_data}</div>
+        ))} */} 
+      {data?.message}
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          // multiple
+          name="file"
+          className="myform"
+          // onChange={handleFileChange}
+          accept="audio/wav"
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
